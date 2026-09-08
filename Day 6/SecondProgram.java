134784c53486f87032c5f8015346eafa2d49438a
Abhinav53 <abhinavkumar.551103@gmail.com>
2026-09-08 23:13:56 +0530

    Add files via upload

---
+++ Day 6/SecondProgram.java
@@ -0,0 +1,13 @@
+class SecondProgram{
+	public static void checkPrime(){
+	System.out.println("This is checkPrime Method");
+	}
+	public static void main(String[] args){
+	checkPrime();
+	add();
+	checkPrime();
+	}
+	public static void add(){
+	System.out.println("This is add Method");
+	}
+}
